da0d3c450cab055ee7ba666db7ba5912de6e00a7
zakaryaahmad1 <133588377+zakaryaahmad1@users.noreply.github.com>
2024-09-20 17:32:22 +0300

    dd

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-module.exports = {
-    eslint: {
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.1
-      ignoreDuringBuilds: true,
-    },
-  }
+
 export default nextConfig;
